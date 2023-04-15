7c944a095bcd1412e4b4c4d54f8d9f5d1482e91f
luccie-cmd <115068339+luccie-cmd@users.noreply.github.com>
2023-04-15 18:03:20 +0200

    Update player.h

--- include/player.h
+++ include/player.h
@@ -5,4 +5,13 @@
 class Player{
     public: 
         Player(Vec2i pos, SDL_Texture *texture);
+        void setX(int x);
+        void setY(int y);
+        void setPos(int x, int y);
+        int getX();
+        int getY();
+        Vec2i getPos();
+    private:
+        Vec2i pos;
+        SDL_Texture* texture;
 };
